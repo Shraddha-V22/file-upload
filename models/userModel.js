053019c298c71926c6adda26dacd9be0e4d955eb
Shraddha-V22 <shraddhav1935@gmail.com>
2023-03-06 17:41:19 +0530

    create user model

---
+++ models/userModel.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require("sequelize");
+const { createDB } = require("../config/db");
+
+const User = createDB.define("user", {
+  id: {
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true,
+    type: DataTypes.INTEGER,
+  },
+  name: DataTypes.STRING,
+  email: DataTypes.STRING,
+  password: DataTypes.STRING,
+  isSeller: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+});
+
+module.exports = User;
